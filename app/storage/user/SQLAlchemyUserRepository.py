9db1244af7bd496c410115e6eebb9659ab22a5ae
qjiujiu <1006485793@qq.com>
2025-10-22 21:57:56 +0800

    fix: 将Pydantic对象转为Dict，以便输出JSON格式

--- app/storage/user/SQLAlchemyUserRepository.py
+++ app/storage/user/SQLAlchemyUserRepository.py
@@ -3,38 +3,7 @@ from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate, UserOut, BatchUsersOut
 from app.storage.user.user_interface import IUserRepository
 from typing import Optional, List, Dict, Union
-from contextlib import contextmanager
-import logging
-
-""" 此处涉及的几个要素:
- 
-    - 数据事务
-
-    
-    - 几种异常抛出方式
-        1. raise
-            直接抛出当前异常，保留原始异常栈信息
-        2. raise e       
-            直接抛出当前异常，但是在之前的信息会丢失
-        3. raise Exception("message") from e  
-            附带额外的报错信息 "message"，同时保留原始异常作为上下文, 形成一大串异常链
-        4. raise ValueError("message") 
-            只抛出当前异常栈信息 (在此之前的调用栈信息不被抛出), 终端给出报错信息会相对少一点
-            
-    数据访问层只抛异常，不对异常进行过多的处理，异常的处理交给上层来解决(通常是接口层, 或者业务逻辑层, 取决于设计者)
-"""
-
-# 使用 Python上下文管理器来处理数据库事务, 在失败的时候自动回滚, 通过事务管理器来保证原子性
-# 成功时提交事务(commit),  commit 完成之后才能调用 refresh 获取最新状态;  失败时回滚事务, 打印异常调用栈，并且重新向上抛出异常!
-@contextmanager
-def transaction(db: Session):
-    try:
-        yield db
-        db.commit()                # commit() 方法用于提交事务，将对数据库的更改保存到数据库中（将插入对象user提交保存）
-    except Exception as e:
-        logging.exception(e)
-        db.rollback()
-        raise
+from app.core.db import transaction
 
 
 # 这是 PySQL+SQLAlchemy 实现的用户仓库(业务逻辑传入的参数是一个 IUserRepository 类型，而不是具体的子类)
@@ -64,6 +33,7 @@ class SQLAlchemyUserRepository(IUserRepository):
         # 获取总记录数
         total = self.db.query(User).count()                     # count() 方法用于返回查询结果的总记录数，通常用于分页计算总数
         # 返回分页后的用户信息 (由的BatchUsersOut 是一个嵌套的 Pydantic BaseModel, 故需要手动调用一下 model_dump 方法来做序列的)
+        # model_dump() 把 Pydantic 对象转为 Dict
         return BatchUsersOut(total=total, count=len(users), users=users).model_dump()
 
     def create_user(self, user_data: UserCreate) -> UserOut:
@@ -85,7 +55,7 @@ class SQLAlchemyUserRepository(IUserRepository):
         # 调用 refresh 处理的实体，必须是已经在库内的实体(因而创建操作需要先做commit 才能刷新)
         for user in user_orms:
             self.db.refresh(user)
-        return [UserOut.model_validate(user) for user in user_orms]
+        return [UserOut.model_validate(user).model_dump() for user in user_orms]
 
     def update_user(self, student_id: str, user_data: UserUpdate) -> Optional[UserOut]:
         user = self.db.query(User).filter(User.student_id == student_id).first()
