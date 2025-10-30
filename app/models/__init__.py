b859ddafe6389c4a2cc477a36c1028afaab017fa
qjiujiu <1006485793@qq.com>
2025-10-30 15:36:12 +0800

    2025.10.30

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@
 from app.models.user import User
 from app.models.order import Order
 from app.models.book import Book  
-
+from app.models.user_restrictions import UserRestriction
 __all__ = ["User", "Order", "Book"]
