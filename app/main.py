4298ae790ff6b4f4e3ace012423cd7a413ed8365
qjiujiu <1006485793@qq.com>
2025-10-30 15:35:27 +0800

    feat: 添加 order 路由注册

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from app import models
-from app.routers import books, users
+from app.routers import books, users, orders
 
 app = FastAPI(title="Library Management System")
 
 # 注册路由
 app.include_router(books.books_router)
 app.include_router(users.users_router)
-# app.include_router(orders.router)
+app.include_router(orders.orders_router)
 
 @app.get("/")
 def root():
