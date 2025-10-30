3c78864b8d408cd7e08721a1f07b96d94adf7f09
qjiujiu <1006485793@qq.com>
2025-10-30 15:23:03 +0800

    style: 添加注释

--- app/core/biz_response.py
+++ app/core/biz_response.py
@@ -1,6 +1,6 @@
 from fastapi.responses import JSONResponse
 from typing import Any, Optional
-
+from app.core.logx import logger
 
 
 # 目前先不区分 code/status_code 区别，后续需要更复杂的业务逻辑时再区分
@@ -8,7 +8,7 @@ class BizResponse(JSONResponse):
     def __init__(
         self, data: Optional[Any] = None, msg: str = "success", status_code: int = 200, 
     ):
-        
+        # logger.debug(data)
         # 目前先让 HTTP 状态码作为业务状态码保持一致即可
         code = status_code
         content = {
