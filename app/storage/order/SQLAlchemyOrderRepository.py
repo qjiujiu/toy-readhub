c5ddd5d8b37201c1a95189d6d05981d36d5d6fda
qjiujiu <1006485793@qq.com>
2025-11-07 16:11:05 +0800

    feat: 借阅订单数据层

--- app/storage/order/SQLAlchemyOrderRepository.py
+++ app/storage/order/SQLAlchemyOrderRepository.py
@@ -5,14 +5,16 @@ from app.schemas.order import OrderCreate, OrderOut, OrderUpdate, BatchOrdersOut
 from app.storage.order.order_interface import IOrderRepository
 from typing import Optional, List
 from app.core.db import transaction
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from app.schemas.order import OrderStatus
 from app.core.logx import logger
+from sqlalchemy.orm import joinedload
 
 class SQLAlchemyOrderRepository(IOrderRepository):
     def __init__(self, db: Session):
         self.db = db
 
+    # 创建借阅订单
     def create_order(self, order_data: OrderCreate) -> OrderOut:
         """创建新的借阅订单"""
         new_order = Order(
@@ -31,7 +33,7 @@ class SQLAlchemyOrderRepository(IOrderRepository):
 
         # 调用 calculate_return_time 计算还书时间
         borrow_time = new_order.borrow_time or datetime.utcnow()
-        return_time = new_order.calculate_return_time(borrow_time, borrow_duration_seconds=30)  # 30秒默认时长
+        return_time = new_order.calculate_return_time(borrow_time)  # 30秒默认时长
         new_order.return_time = return_time  # 设置计算得到的还书时间
 
         # 提交事务（如果创建时有修改 return_time）
@@ -39,6 +41,7 @@ class SQLAlchemyOrderRepository(IOrderRepository):
             self.db.commit()
         return OrderOut.model_validate(new_order).model_dump() 
     
+    # 获取所有订单
     def get_batch_orders(self, page: int, page_size: int) -> BatchOrdersOut:
         # 获取借阅订单列表并分页
         orders = (
@@ -51,30 +54,41 @@ class SQLAlchemyOrderRepository(IOrderRepository):
         
         return BatchOrdersOut(total=total, count=len(orders), orders=[OrderOut.model_validate(order) for order in orders])
     
+    # 根据订单ID oid 获取借阅订单
     def get_order_by_oid(self, order_id: int) -> Optional[OrderOut]:
         order = self.db.query(Order).filter(Order.order_id == order_id).first()
-        return OrderOut.model_validate(order).model_dump() if order else None
+        return OrderOut.model_validate(order) if order else None
 
-    def get_orders_by_uid(self, user_id: int, page: int, page_size: int) -> List[OrderOut]:
+    
+    # 根据用户ID uid 获取借阅订单
+    def get_orders_by_uid(self, user_id: int) -> List[OrderOut]:
         # 获取特定用户的借阅记录
         orders = (
             self.db.query(Order)
             .filter(Order.user_id == user_id)
-            .offset(page * page_size)
-            .limit(page_size)
             .all()
         )
         return [OrderOut.model_validate(order) for order in orders]
     
-    def get_order_by_bid(self, book_id: int, page: int, page_size: int)->List[OrderOut]:
+    # 根据图书ID bid 获取借阅订单
+    def get_order_by_bid(self, book_id: int)-> OrderOut:
+        order = self.db.query(Order).filter(Order.book_id == book_id).first()
+        if not order:
+            return None
+        return OrderOut.model_validate(order)
+
+    # 根据图书状态 status 获取借阅订单
+    def get_orders_by_status(self, status: OrderStatus, page: int = 0, page_size: int = 10) -> BatchOrdersOut:
+        # 查询符合状态的订单
+        total = self.db.query(Order).filter(Order.status == status).count()
         orders = (
             self.db.query(Order)
-            .filter(Order.book_id == book_id)
+            .filter(Order.status == status)
             .offset(page * page_size)
             .limit(page_size)
             .all()
         )
-        return [OrderOut.model_validate(order) for order in orders]
+        return BatchOrdersOut(total=total, count=len(orders), orders=[OrderOut.model_validate(order) for order in orders])
 
     def update_order(self, order_id: str, order_data: OrderUpdate) -> Optional[OrderOut]:
         # 更新借阅订单状态或归还时间
@@ -87,38 +101,44 @@ class SQLAlchemyOrderRepository(IOrderRepository):
 
             # 根据状态处理归还时间
             if order_data.status == OrderStatus.returned:
-                order.return_time = datetime.utcnow()             # 如果状态是归还，设置归还时间为当前时间
-            elif order_data.status == OrderStatus.lost_and_returned:
-                order.return_time = datetime.utcnow()             # 如果是丢失后归还，也设置归还时间为当前时间
-                                                                  # 如果是丢失(lost)状态，则不更新归还时间
+                order.actual_return_time = datetime.now(timezone(timedelta(hours=8)))             # 如果状态是归还，设置归还时间为当前时间
+
             with transaction(self.db):
                 self.db.commit()  # 提交更改
 
-            return OrderOut.model_validate(order).model_dump()
+            return OrderOut.model_validate(order)
 
         return None
-
     
 
-
-
-
-
-
-
-    # TODO 下面功能待完善
-    
-
-    
-
-
-    def delete_order(self, order_id: str) -> None:
-        # 删除借阅订单
-        order = self.db.query(Order).filter(Order.order_id == order_id).first()
+    # 删除借阅订单
+    def delete_order(self, order_id: int) -> OrderOut:
+        order = self.db.query(Order).options(joinedload(Order.book), joinedload(Order.user)).filter(Order.order_id == order_id).first()
+        # 使用 joinedload 来确保 book 和 user 在删除之前就被加载
+        # 否者，删除order之后就无法加载关联的book和user，响应体OrderOut处会报错
         if order:
             # 使用事务管理器删除订单
             with transaction(self.db):
                 self.db.delete(order)  # 从会话中删除订单
                 self.db.commit()  # 提交更改
-
+            return OrderOut.model_validate(order).model_dump() # 返回已删除订单的详细信息
+        return None
+    
+    def delete_orders_by_status(self, status: OrderStatus) -> BatchOrdersOut:
+        # 查询所有符合指定状态的订单，并加载关联的 book 和 user
+        orders = self.db.query(Order).options(joinedload(Order.book), joinedload(Order.user)).filter(Order.status == status).all()
+        total = len(orders)  # 获取符合条件的订单总数
+        if not orders:
+            return None  # 如果没有符合条件的订单，返回空列表
+        
+        deleted_orders = []
+        
+        # 使用事务管理器删除订单
+        with transaction(self.db):
+            for order in orders:
+                self.db.delete(order)  # 从会话中删除订单
+                deleted_orders.append(OrderOut.model_validate(order).model_dump())  # 将已删除的订单添加到删除结果中
+            self.db.commit()  # 提交更改
+        
+        return BatchOrdersOut(total=total, count=len(deleted_orders), orders=deleted_orders)
     
--- app/storage/order/order_interface.py
+++ app/storage/order/order_interface.py
@@ -1,23 +1,32 @@
 from typing import Protocol, Optional, List
-from app.schemas.order import OrderCreate, OrderOut, OrderUpdate, BatchOrdersOut
+from app.schemas.order import OrderCreate, OrderOut, OrderUpdate, BatchOrdersOut, OrderStatus
 
 class IOrderRepository(Protocol):
     # 根据订单号oid查询借阅订单
     def get_order_by_oid(self, order_id: str) -> Optional[OrderOut]:
         ...
+
     # 根据用户uid查询借阅订单
-    def get_orders_by_uid(self, user_id: int, page: int, page_size: int) -> List[OrderOut]:
-        ...
-    def get_order_by_bid(self, book_id: int, page: int, page_size: int)->List[OrderOut]:
+    def get_orders_by_uid(self, user_id: int) -> List[OrderOut]:
         ...
 
+    # 根据图书ID uid 查询借阅订单
+    def get_order_by_bid(self, book_id: int)->List[OrderOut]:
+        ...
+
+    # 根据借阅状态 status 查询借阅订单
+    def get_orders_by_status(self, status: OrderStatus, page: int, page_size: int) -> BatchOrdersOut:
+        ...
+        
+    # 创建借阅订单记录
     def create_order(self, order_data: OrderCreate) -> OrderOut:
         ...
 
+    # 更新借阅订单
     def update_order(self, order_id: str, order_data: OrderUpdate) -> Optional[OrderOut]:
         ...
 
-    def delete_order(self, order_id: str) -> None:
+    def delete_order(self, order_id: int) -> OrderOut:
         ...
 
     def get_batch_orders(self, page: int, page_size: int) -> BatchOrdersOut:
