30594ed0e5f5b83bd23c9e179126cd0799d15cd2
qjiujiu <1006485793@qq.com>
2025-11-07 16:13:05 +0800

    初始化

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@
 from app.models.user import User
 from app.models.order import Order
 from app.models.book import Book  
-from app.models.user_restrictions import UserRestriction
+from app.models.user_credit import UserCredit
 __all__ = ["User", "Order", "Book"]
